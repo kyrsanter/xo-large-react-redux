be83cfbfe075a86613bd30980ee52d23133948bf
Admin <you@example.com>
2020-02-05 19:41:02 +0300

    Updates

---
+++ precache-manifest.573f773ea6574f4e2579c4be8195f39a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f7991fc2d8bb1b66c61c563e3fa59a",
+    "url": "/xo-large-react-redux/index.html"
+  },
+  {
+    "revision": "cb5de41c99ffc2563e2c",
+    "url": "/xo-large-react-redux/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "42ccd026e4c02b9bdb9e",
+    "url": "/xo-large-react-redux/static/css/main.8e8a4291.chunk.css"
+  },
+  {
+    "revision": "cb5de41c99ffc2563e2c",
+    "url": "/xo-large-react-redux/static/js/2.14d136fb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/xo-large-react-redux/static/js/2.14d136fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ccd026e4c02b9bdb9e",
+    "url": "/xo-large-react-redux/static/js/main.bd3576dd.chunk.js"
+  },
+  {
+    "revision": "715e4ce80dfb4da2aa93",
+    "url": "/xo-large-react-redux/static/js/runtime-main.ce51c793.js"
+  }
+]);
